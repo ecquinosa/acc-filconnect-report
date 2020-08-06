4593e88bbf17287cab56f59951d9d4b605167347
antpearl2011 <antpearl2011@gmail.com>
2020-08-06 13:33:36 +0800

    enable subscription in routes

--- src/api/routes/report.ts
+++ src/api/routes/report.ts
@@ -18,47 +18,32 @@ export default (app: Router) => {
    * /example/create
    */
   route.post(ROUTE.REPORT.KYC_SEARCH_CITIZEN, async (req: Request, res: Response, next: NextFunction) => {
-    // const validationResult = await exampleService.validateCreateVM(req.body);
-    // if (validationResult.length > 0) {
-    //   // invalid model.
-    //   logger.error("🔥 validation error: %o", validationResult);
-    //   var errrorResponse = {
-    //     responseCode: RESPONSE_CODE.INVALID_PARAMETER.CODE,
-    //     responseMessage: RESPONSE_CODE.INVALID_PARAMETER.MESSAGE,
-    //     responseCount: 0,
-    //     TotalCount: 0,
-    //     response: validationResult,
-    //   };
-    //   return res.status(400).send(errrorResponse);
-    // }
-
     const entity = JSON.parse(JSON.stringify(req.body));
 
-    const result = await kycService.SearchCitizen(entity);
+    //const result = await kycService.SearchCitizen(entity);
     
-    // const result = await camundService.Start(
-    //   JSON.stringify(req.body),
-    //   JSON.stringify(req.headers),
-    //   ORCHESTRATION.PROCESS_DEFINITION.REPORT.KYC_SEARCH_CITIZEN
-    // );
+    const result = await camundService.Start(
+      JSON.stringify(req.body),
+      JSON.stringify(req.headers),
+      ORCHESTRATION.PROCESS_DEFINITION.REPORT.KYC_SEARCH_CITIZEN
+    );    
 
-    //var result = true;
-
-    //logger.info(result);
+    logger.info(result);
     return res.json({ response: result }).status(200);
   });
 
   route.post(ROUTE.REPORT.GET_FILE, async (req: Request, res: Response, next: NextFunction) => {
     const entity = JSON.parse(JSON.stringify(req.body));
 
-    const result = await kycService.getFile(entity);
+    //const result = await kycService.getFile(entity);
     
-    // const result = await camundService.Start(
-    //   JSON.stringify(req.body),
-    //   JSON.stringify(req.headers),
-    //   ORCHESTRATION.PROCESS_DEFINITION.REPORT.KYC_SEARCH_CITIZEN
-    // );
+    const result = await camundService.Start(
+      JSON.stringify(req.body),
+      JSON.stringify(req.headers),
+      ORCHESTRATION.PROCESS_DEFINITION.REPORT.GET_FILE
+    );
     
+    logger.info(result);
     return res.json({ response: result }).status(200);
   });
  
